27919a70991a20492a8834fb6401eeb579bece49
Duaarabie <Duaarabie@gmail.com>
2023-08-11 15:53:40 -0700

    commit all

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,31 +19,26 @@ int main(void)
 	{
 		for (k = 48; k <= 56; k++)
 		{
-			/*putchar(l);
-			putchar(k);
-			putchar(space);*/
 			for (i = 48; i <= 57; i++)
 			{
 				for (j = 48; j <= 57; j++)
 				{
 					if (!( l == 48 && k == 48 && i == 48 && i == 48))
 					{
-				
-							putchar(l);
-							putchar(k);
+						putchar(l);
+						putchar(k);
+						putchar(space);
+						putchar(i);
+						putchar(j);
+						if (!( k == 56 && l == 57 && i == 57 && j == 57))							{
+							putchar(semi);
 							putchar(space);
-							putchar(i);
-							putchar(j);
-							if (!( k == 56 && l == 57 && i == 57 && j == 57))
-							{
-								putchar(semi);
-								putchar(space);
-							}
+						}
 				
+					}
 				}
 			}
 		}
-	}
 	}
 	putchar(newLine);
 	return (0);
