d5c34d889079f8ab7b484a2ad0d736eb0157942b
Duaarabie <Duaarabie@gmail.com>
2023-08-11 13:00:39 -0700

    commit all

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int numbs = 48;
+	short newLine = '\n';
+	int i = 0;
+	int semi = 44;
+	int space = 32;
+
+	while (i < 10)
+	{
+		putchar(numbs);
+		if ( i < 9)
+		{
+			putchar(semi);
+			putchar(space);
+		}
+		i++;
+		numbs++;
+	}
+	putchar(newLine);
+	return (0);
+}
