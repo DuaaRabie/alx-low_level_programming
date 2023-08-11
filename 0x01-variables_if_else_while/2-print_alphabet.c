a854bdb91443bc5bdec4bcf38106eed2587f638f
Duaarabie <Duaarabie@gmail.com>
2023-08-11 09:27:54 -0700

    commit all

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c = 'a';
+	char newLine = '\n';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(newLine);
+	return (0);
+}
