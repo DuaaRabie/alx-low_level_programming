5f82397add7b2ffa9dfd6954db077e6a95aa315e
Duaarabie <Duaarabie@gmail.com>
2023-08-11 17:48:38 -0700

    commit all

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -10,34 +9,26 @@ int main(void)
 	int semi = 44;
 	int space = 32;
 	short newLine = '\n';
-	int i;
-	int j;
-	int k;
 	int l;
+	int k;
 
-	for (l = 48; l <= 57; l++)
+	for (l = 0; l <= 99; l++)
 	{
-		for (k = 48; k <= 56; k++)
+		for (k = 0; k <= 99; k++)
 		{
-			for (i = 48; i <= 57; i++)
+			if ((l % 10) != 0)
 			{
-				for (j = 48; j <= 57; j++)
+				if (l != k)
 				{
-					if (!( l == 48 && k == 48 && j == 48 && i == 48))
+					putchar((l / 10) + 48);
+					putchar((l % 10) + 48);
+					putchar(space);
+					putchar((k / 10) + 48);
+					putchar((k % 10) + 48);
+					if (!(l == 98 && k == 99))
 					{
-						if ((l * 10 + k) < (i * 10 + j))
-						{
-						putchar(l);
-						putchar(k);
+						putchar(semi);
 						putchar(space);
-						putchar(i);
-						putchar(j);
-						if (!( k == 56 && l == 57 && i == 57 && j == 57))							{
-							putchar(semi);
-							putchar(space);
-						}
-						}
-				
 					}
 				}
 			}
