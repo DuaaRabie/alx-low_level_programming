e631139e5a833d4b7b46c308ce4ac18c22b0c6b3
Duaarabie <Duaarabie@gmail.com>
2023-08-15 09:26:08 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,15 +4,15 @@
  * main - Entry point
  *
  * Return: 0 for Success
- */
+*/
 int main(void)
 {
 	char output[] = "_putchar\n";
 	int i;
-	
-	for (i=0; i<9; i++)
+
+	for (i = 0; i < 9; i++)
 	{
 		_putchar(output[i]);
 	}
-	return 0;
+	return (0);
 }
