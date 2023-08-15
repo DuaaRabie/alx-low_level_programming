01d76442afe54bbd9628735888e1b4f56df648b9
Duaarabie <Duaarabie@gmail.com>
2023-08-15 15:02:34 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,17 +16,23 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{ 
 			k = j * i;
-			if (k > 9)
+			if (!(i != 0 || j != 0))
 			{
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				if (k > 9)
+				{
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
 			}
 			else
 			{
-				if ( i !=0)
-				{
-					_putchar(' ');
-				}
 				_putchar(k + '0');
 			}
 			if (j != 9)
