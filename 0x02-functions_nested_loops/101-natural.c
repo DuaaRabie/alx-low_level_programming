b68c67dbf0c2a0cdcf6d5f4a94a66a3edaa10adb
Duaarabie <Duaarabie@gmail.com>
2023-08-15 17:58:07 -0700

    natural for 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int multi3;
+	int multi5;
+	int sum = 0;
+
+	for (multi3 = 0; multi3 < 1024; multi3++)
+	{
+		if (multi3 % 3 == 0)
+		{
+			sum += multi3;
+		}
+	}
+	for (multi5 = 0; multi5 < 1024; multi5++)
+	{
+		if (multi5 % 5 == 0)
+		{
+			sum += multi5;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
