755bb056a990a5ef4c12d9e6518ef286f5b865ae
Duaarabie <Duaarabie@gmail.com>
2023-08-15 17:32:17 -0700

    time table of n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,16 +2,16 @@
 #include "main.h"
 /**
  * print_times_table - prints the n times table
- *
+ * @n: time table number
  * Return: void
 */
 void print_times_table(int n)
 {
-	int i ;
-	int j ;
-	int k ;
+	int i;
+	int j;
+	int k;
 
-	if (!(n > 15 || n < 0 ))
+	if (!(n > 15 || n < 0))
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -20,21 +20,21 @@ void print_times_table(int n)
 				k = j * i;
 				if (k > 99)
 				{
-					printf(" %d",k);
+					printf(" %d", k);
 				}
 				else if (k > 9)
 				{
-					printf("  %d",k);
+					printf("  %d", k);
 				}
 				else
 				{
 					if (!(j == 0))
 					{
-						printf("   %d",k);
+						printf("   %d", k);
 					}
 					else
 					{
-						printf("%d",k);
+						printf("%d", k);
 					}
 				}
 				if (j != n)
@@ -43,6 +43,6 @@ void print_times_table(int n)
 				}
 			}
 			printf("\n");
-		}		
+		}
 	}
 }
