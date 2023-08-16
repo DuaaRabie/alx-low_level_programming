e310a6398b1968446dcfdc4fcd07cd4dcca2d0a7
Duaarabie <Duaarabie@gmail.com>
2023-08-15 18:19:51 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int f1 = 0;
+	int f2 = 1;
+	int f;
+
+	for (f = 1;  f <= 50; f++)
+	{
+		f = f1 + f2;
+		f1 = f2;
+		f2 = f;
+		if (f == 50)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
