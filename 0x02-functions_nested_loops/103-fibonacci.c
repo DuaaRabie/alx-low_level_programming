38f45841c9ad0986b7d25466091381abef1841bf
Duaarabie <Duaarabie@gmail.com>
2023-08-15 18:57:28 -0700

    fabonacci2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,18 +9,18 @@ int main(void)
 	unsigned long f1 = 0;
 	unsigned long f2 = 1;
 	unsigned long f = 0;
-	unsigned long sum = 0;
+	float sum = 0;
 
 	while(f <= 4000000)
 	{
 		f = f1 + f2;
-		if (f % 2 == 0)
+		if ((f % 2) == 0)
 		{
 			sum += f;
 		}
 		f2 = f;
 		f1 = f2;
 	}
-	printf("%lu\n",sum);
+	printf("%.0f\n",sum);
 	return (0);
 }
