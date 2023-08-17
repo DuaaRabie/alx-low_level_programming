b9b490ddf5c0a00ee910058c84037d520e2e0456
Duaarabie <Duaarabie@gmail.com>
2023-08-17 08:47:28 -0700

    prints numbers 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - checks upper case characters
+ * Return: void
+*/
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+}
