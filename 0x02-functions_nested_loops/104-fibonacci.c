110fd58282d206d6367d76dd6578adb69b0b5ca0
Duaarabie <Duaarabie@gmail.com>
2023-08-16 19:02:54 -0700

    fibonacci3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,28 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * Length - return the length of the number
+ *
+ * @n: the number
+ *
+ * Return: the length
+*/
+int Length(int n)
+{
+	int length = 1;
+
+	if (!n)
+		return (1);
+	while (n)
+	{
+		n = n / 10;
+		length += 1;
+	}
+	return (length);
+}
+
+
 /**
  * main - entry point
  *
@@ -6,22 +30,36 @@
 */
 int main(void)
 {
-	unsigned long f1 = 0;
-	unsigned long f2 = 1;
-	unsigned long f;
-	int i;
+	unsigned long f1 = 1, f2 = 2;
+	unsigned long s1 = 0, s2 = 0;
+	unsigned long sum1, sum2, value = 100000000;
+	int i, seperator;
 
 	for (i = 0; i < 99; i++)
 	{
-		f = f1 + f2;
+		if (s1 > 0)
+		{
+			printf("%lu", s1);
+		}
+		seperator = Length(value) - 1 - numLength(f1);
+		while (s1 > 0 && seperator > 0)
+		{
+			printf("%d", 0);
+			seperator--;
+		}
 		printf("%lu", f);
+		sum1 = (f1 + f2) % value;
+		sum2 = s1 + s2 + (f1 + f2) / value;
 		f1 = f2;
-		f2 = f;
+		s1 = s2;
+		f2 = sum1;
+		s2 = sum2;
+
 		if (i != 98)
 		{
 			printf(", ");
 		}
+		printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
