99edd1976ac45ce51440d01a253989bb3075fadc
Duaarabie <Duaarabie@gmail.com>
2023-08-17 16:30:26 -0700

    print numbers for 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,30 +7,28 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	char n;
 	char n1;
 	char n2;
 
 	for (j = 0; j < 10; j++)
 	{
-		n = 48;
 		n1 = 48;
-		n2 = 49;
-
+		n2 = 47;
 		for (i = 0; i < 15; i++)
 		{
-			if (i < 10)
-			{
-				_putchar(n);
-				n++;
-			}
 			if (i >= 10)
 			{
-				_putchar(n2);
-				_putchar(n1);
-				n1++;
+				n1 = 49;
+				n2++;
 			}
+			_putchar(n1);
+			if (i >= 10)
+			{
+			_putchar(n2);
+			}
+			n1++;
 		}
+
 	_putchar('\n');
 	}
 }
