97c3c67019ac7410c1dee52090d5557404e4d378
Duaarabie <Duaarabie@gmail.com>
2023-08-17 16:51:42 -0700

    prints line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: number of _
+ * Return: void
+*/
+void print_line(int n)
+{
+	int i;
+	
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
