5ffb19a4e2c0ad42dcd110f3c5c2323ddfd5a80e
Duaarabie <Duaarabie@gmail.com>
2023-08-17 18:20:54 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/other.c
+++ 0x04-more_functions_nested_loops/other.c
@@ -7,27 +7,36 @@
 void fizz_buzz(void)
 {
 	int i;
-	int n = 1; 
+	int n = 1;
+	int w = 8;
+	char word[w]; 
+
 
 	for (i = 0; i <= 99; i++)
 	{
+		if ((n % 3 ==0) || (n % 5 == 0))
+		{
 			if ((n % 3 == 0) && !(n % 5 == 0))
 			{
-				printf("Fizz ");
+				w = 4;
+				word[] = "Fizz";
 			}
 			else if (!(n % 3 == 0) && (n % 5 == 0))
 			{
-				printf("Buzz ");
+				w = 4;
+				word[] = "Buzz";
 			}
 			else if ((n % 3 == 0) && (n % 5 == 0))
 			{
-				printf("FizzBuzz ");
+				word[] = "FizzBuzz"
 			}
+			printf("%s ", word);
+		}
 		else
 		{
 			printf("%d ", n);
 		}
-		n++;
+		n++
 	}
 	printf("\n");
 }
