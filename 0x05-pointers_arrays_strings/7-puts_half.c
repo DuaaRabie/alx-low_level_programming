fee53bdfbf3b30b24eb05d3209d29ffd71db4a76
Duaarabie <Duaarabie@gmail.com>
2023-08-21 18:45:35 -0700

    commit all

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str: address of a string
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int count;
+	int length = 0;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		length += 1;
+	}
+	if (length % 2 == 0)
+	{
+		for (count = length / 2; count <= length; count++)
+		{
+			_putchar(str[count]);
+		}
+	}
+	else
+	{
+		for (count = ((length - 1) / 2); count <= length; count++)
+		{
+			_putchar(str[count]);
+		}
+	}
+	_putchar('\n');
+}
