0a0e9970bbb4223a9880ee8946bb8a22304a9623
Duaarabie <Duaarabie@gmail.com>
2023-08-21 18:25:15 -0700

    commit all

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: address of a string
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] > '\0'; count++)
+	{
+		if ((str[count] % 2) == 0)
+		{
+			_putchar(str[count]);
+		}
+	}
+	_putchar('\n');
+}
