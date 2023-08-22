c02fecc63acd6b3c212a63c2cd539c9b77ed7c62
Duaarabie <Duaarabie@gmail.com>
2023-08-21 20:00:45 -0700

    commit all

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,11 +11,10 @@ int _atoi(char *s)
 	int sign = 1;
 	int number;
 
-	do
-	{
+	do {
 		if (*s == '-')
 			sign *= -1;
-		else if (*s >= '0' && *s <='9')
+		else if (*s >= '0' && *s <= '9')
 			n = (n * 10) + (*s - '0');
 		else if (n > 0)
 			break;
