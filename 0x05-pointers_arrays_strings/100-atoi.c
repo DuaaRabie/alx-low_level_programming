cf72f4c4829ad014d7a1f8e10aa85ae7c1539228
Duaarabie <Duaarabie@gmail.com>
2023-08-21 19:57:50 -0700

    commit all

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string to number
+ * @s: address of a string
+ * Return: nothing
+ */
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+	int sign = 1;
+	int number;
+
+	do
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <='9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+	number = n * sign;
+
+	return (number);
+}
