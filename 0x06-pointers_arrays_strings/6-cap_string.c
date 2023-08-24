a98b7452416279b3b961b9887a7f8c8babb4a752
Duaarabie <Duaarabie@gmail.com>
2023-08-24 08:49:57 -0700

    commit all

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,6 +17,8 @@ char *cap_string(char *str)
 			if (str[i] == del[j])
 				if(str[i+1] > 96 && str[i+1] < 123)
 					str[i + 1] -= 32;
+			if (str[i] == '\t')
+				str[i] = ' ';
 		}
 	}
 	return (str);
