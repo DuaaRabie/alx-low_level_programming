cc3da7c7e0a9b6e43de00acae14194b975004c20
Duaarabie <Duaarabie@gmail.com>
2023-08-24 07:15:34 -0700

    commit all

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *string_toupper - change all characters to uppercase
+ * @str: address of string
+ * Return: pointer to uppercase string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] > 97 && str[i] < 123)
+			str[i] -= 32;
+	}
+	return (str)
+}
