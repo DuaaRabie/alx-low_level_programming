e0561df3546aca9b9e15a3c263598c9b3390f3e3
Duaarabie <Duaarabie@gmail.com>
2023-08-24 09:45:04 -0700

    commit all

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *rot13 - encode to rot13
+ * @str: address of string
+ * Return: pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+	char s1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char s2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; s1[j] != '\0'; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
