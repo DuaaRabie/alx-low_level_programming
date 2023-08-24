1481e819636eb4788cb09778e8265b23e91cff21
Duaarabie <Duaarabie@gmail.com>
2023-08-24 04:33:45 -0700

    commit all

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -31,17 +31,20 @@ int _strcmp(char *s1, char *s2)
 {
 	int sum1 = strsum(s1);
 	int sum2 = strsum(s2);
+	int def = sum1 - sum2;
 
-	if (sum1 == sum2)
+	/*if (sum1 == sum2)
 	{
-		return (0);
+		return (s);
 	}
 	else if (sum1 < sum2)
 	{
 		return (-15);
 	}
-	else
+	else if (sum1 > sum2)
 	{
 		return (15);
 	}
+	else*/
+	return (def);
 }
