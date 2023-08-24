87ba326b8a279036afa0faaafec6e598df3f4650
Duaarabie <Duaarabie@gmail.com>
2023-08-24 08:14:46 -0700

    commit all

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *cap_string - change all characters to uppercase
+ * @str: address of string
+ * Return: pointer to capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char del[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; del[j] != '\0'; j++)
+		{
+			if (str[i] == del[j])
+			{
+				if (str[i] > 96 && str[i] < 123)
+					str[i + 1] -= 32;
+			}
+		}
+	}
+	return (str);
+}
