1803fdd2588a6a62e4e272b537da176064a0ee4a
Duaarabie <Duaarabie@gmail.com>
2023-08-24 16:14:32 -0700

    commit all

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print buffer - print contents of a buffer
+ * @b: the buffer
+ * @size: size of the buffer
+ * Return: nothing
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%c", b[j]);
+		}
+		printf("\n");
+	}
+}
