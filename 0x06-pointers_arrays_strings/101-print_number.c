b05485e41c8b98ef5496009b60b58b2aab912cf8
Duaarabie <Duaarabie@gmail.com>
2023-08-24 11:53:04 -0700

    commit all

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - check the code
+ * @n: number
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int m = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -m;
+	}
+
+	if ((m / 10) > 0)
+	{
+		print_number(m / 10);
+	}
+
+	_putchar((m % 10) + 48);
+}
