feb329cae5d0e39c9b8b0d86ea82c948d97b9b12
Duaarabie <Duaarabie@gmail.com>
2023-08-27 09:20:37 -0700

    commit all

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,9 +11,8 @@ void print_buffer(char *b, int size)
 {
 	int i, j, k;
 	int count = 0, remain = size;
-
 	if (remain == 0)
-		printf("%c",'\0');
+		printf("\n");
 	for (i = 1; i <= 10; i++)
 	{
 		while (remain >= 1)
