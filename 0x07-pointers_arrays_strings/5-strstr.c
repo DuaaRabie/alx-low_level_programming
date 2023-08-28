89cf3101934e5a495962dfaf80b17afb0a00113e
Duaarabie <Duaarabie@gmail.com>
2023-08-28 10:58:36 -0700

    commit all

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * length - compute the length of string
+ * @str: address to string
+ * Return: the length
+ */
+unsigned int length(char *str)
+{
+	unsigned int len = 0;
+
+	while (*str++ != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+
+/**
+ * *_strstr - locate a substring
+ * @haystack: address to the examied string
+ * @needle: address to refrence substring
+ * Return: pointer to the first match | NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int reflen = length(needle);
+	unsigned int slen = length(haystack);
+	unsigned int i, j;
+	int count = 0;
+	int *result;
+
+	for (i = 0; i < reflen; i++)
+	{
+		for (j = ; j < slen; j++)
+		{
+			if (*(haystack + i) == *(needle + j))
+			{
+				count++;
+			}
+			else
+				count--;
+			if (count == 1)
+				result = haystack + i;
+
+		}
+	}
+
+	if (count == slen - 1)
+		return (result);
+	else
+		return ('\0');
+
+
+}
+
