d600e3acb8b9b3d5daed7009abaa6cbfed7431b0
Duaarabie <Duaarabie@gmail.com>
2023-08-28 14:13:29 -0700

    commit all

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @a: address of the array
+ * Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
