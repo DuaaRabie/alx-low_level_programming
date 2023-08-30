f375455463b75c5bde399ac33b72fb1aaee19f0c
Duaarabie <Duaarabie@gmail.com>
2023-08-30 15:02:51 -0700

    commit all

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,28 @@
  #include "main.h"
 
 /**
+ * square_root - compute if sq is the square of the num
+ * @sq: tested number
+ * @num: the number needs the square root of
+ * Return: square root
+ */
+int square_root(int num, int sq)
+{
+	if (sq * sq == num)
+		return (sq);
+	else if (sq * sq > num)
+		return (square_root(num, --sq));
+	return (-1);
+}
+
+/**
  * _sqrt_recursion - compute the natural square root of a number
  * @n: the number
  * Return: the natural square root
  */
 int _sqrt_recursion(int n)
 {
-	int sq = n / 2;
-
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
-		return (0);
-	else if (n == 1)
-		return (1);
-	while (sq)
-	{
-		if (sq * sq == n)
-			return (sq);
-		sq--;
-	}
-	return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (square_root(n, n / 2));
 }
