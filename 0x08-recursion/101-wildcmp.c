8c67a9af80d339716fbcce9334634c0d718c66de
Duaarabie <Duaarabie@gmail.com>
2023-08-31 16:45:28 -0700

    commit all

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -31,9 +31,9 @@ int wildcmp(char *s1, char *s2)
 		}
 		else if (l1 > l2)
 		{
-			if (s1[l1 - l2 + 1] == s2[1])
-				return (wildcmp(s1 + l1 - l2 + 1, s2 + 1));
-			else if (s1[1] == s2[1])
+			/*if (s1[l1 - l2 + 1] == s2[1])
+				return (wildcmp(s1 + l1 - l2 + 1, s2 + 1));*/
+			if (s1[1] == s2[1])
 				return (wildcmp(s1 + 1, s2 + 1));
 			return (wildcmp(s1 + 1, s2));
 		}
