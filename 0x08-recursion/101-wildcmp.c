8bc7c5d23dd4d736c626574c990076fb024e6e09
Duaarabie <Duaarabie@gmail.com>
2023-08-31 16:43:01 -0700

    commit all

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,68 +10,41 @@ int wildcmp(char *s1, char *s2)
 {
 	int l1 = strlen(s1), l2 = strlen(s2);
 
-	if (l1 == 1 && l2 != 1)
+	if (l1 == 1)
 	{
-		if (s1[0] == s2[0])
-			return (wildcmp(s1, s2 + 1));
-		if (s2[0] == '*')
-		{
-			if (s1[0] == s2[1])
-				return (wildcmp(s1, s2 + 1));
-			return (wildcmp(s1, s2 + 1));
-		}
-		else
-			return (0);
-	}
-
-	if (l1 == 1 && l2 == 1)
-	{
-		if (s1[0] == s2[0] || s2[0] == '*')
+		if (l2 == 1 && (s1[0] == s2[0] || s2[0] == '*'))
 			return (1);
+		else if (l2 != 1 && (s1[0] == s2[0] || s2[0] == '*'))
+			return (wildcmp(s1, s2 + 1));
 		else
 			return (0);
 	}
-
-	if (l1 == l2)
+	else if (s1[0] == s2[0])
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (s2[0] == '*')
 	{
-		if (s1[0] == s2[0])
-			return (wildcmp(s1 + 1, s2 + 1));
-		else if (s2[0] == '*')
+		if (l1 == l2)
 		{
-			if (s1[1] == s2[1])
+			if (s1[1] == s2[1] || s2[1] == '*')
 				return (wildcmp(s1 + 1, s2 + 1));
-			else
-				return (wildcmp(s1 + 1, s2));
+			return (wildcmp(s1 + 1, s2));
 		}
-		return (0);
-	}
-	else if (l1 > l2)
-	{
-		if (s1[0] == s2[0])
-			return (wildcmp(s1 + 1, s2 + 1));
-		else if (s2[0] == '*')
+		else if (l1 > l2)
 		{
 			if (s1[l1 - l2 + 1] == s2[1])
 				return (wildcmp(s1 + l1 - l2 + 1, s2 + 1));
-			else if (s1[0] == s2[1])
+			else if (s1[1] == s2[1])
 				return (wildcmp(s1 + 1, s2 + 1));
-			else
-				return (wildcmp(s1 + 1, s2));
+			return (wildcmp(s1 + 1, s2));
 		}
-		else
-			return (0);
-	}
-	else
-	{
-		if (s1[0] == s2[0])
-			return (wildcmp(s1 + 1, s2 + 1));
-		else if (s2[0] == '*')
+		else if (l1 < l2)
 		{
 			if (s1[0] == s2[1])
 				return (wildcmp(s1, s2 + 1));
-			return (wildcmp(s1 + 1, ++s2));
+			else if (s1[1] == s2[1] || s2[1] == '*')
+				return (wildcmp(s1 + 1, s2 + 1));
+			return (wildcmp(s1 + 1, s2));
 		}
-		else
-			return (0);
 	}
+	return (0);
 }
