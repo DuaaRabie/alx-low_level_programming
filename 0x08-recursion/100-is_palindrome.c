f02d7e075b98d651819336602e836b4764e4d8d9
Duaarabie <Duaarabie@gmail.com>
2023-08-30 17:11:54 -0700

    commit all

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+ #include "main.h"
+
+/**
+ * simetric_check - check the start and the end of a string
+ * @s: address to string
+ * @length: string length
+ * Return: 1 if simetric || 0 if not simetric
+ */
+int simetric_check(char *s, int length)
+{
+	if (length == 1 || length == 2)
+	{
+		if (s[0] == s[length])
+			return (1);
+		else
+			return (0);
+	}
+	else
+	{
+		if (s[0] == s[length])
+		{
+			return (simetric_check(s + 1, length - 2));
+		}
+		else
+			return (0);
+	}
+
+}
+/**
+ * is_palindrome - check if the string is a palindrome
+ * @s: address to the string
+ * Return: 1 if string palindrome || 0 if not
+ */
+int is_palindrome(char *s)
+{
+	int last = -1;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		last++;
+		i++;
+	}
+
+	return (simetric_check(s, last));
+}
