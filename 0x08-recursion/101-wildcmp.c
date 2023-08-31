bf45c67cb7140997d12142dc917d2bc5c3585dd3
Duaarabie <Duaarabie@gmail.com>
2023-08-30 17:59:27 -0700

    commit all

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,35 @@
+ #include "main.h"
+
+/**
+ * wildcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if identical || 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int l1 = strlen(s1);
+	int l2 = strlen(s2);
+
+	if (l1 == l2)
+	{
+		if (s1[0] == s2[0])
+			return (wildcmp(s1 - 1, s2 - 1));
+
+		else if (s2[0] == '*')
+			return (wildcmp(s1 - 1, s2 - 1));
+		return (0);
+	}
+	else
+	{
+		if (s2[0] == '*')
+		{
+			if (l2 < l1)
+				return (wildcmp(s1 - 1, s2));
+			else
+				return (wildcmp(s1 - 1, ++s2));
+		}
+
+		return (0);
+	}
+}
