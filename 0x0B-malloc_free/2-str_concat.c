047b97f9f8002c297729154a14ebbae1f5843b3d
Duaarabie <Duaarabie@gmail.com>
2023-09-04 11:47:51 -0700

    commit all

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,16 +9,23 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	unsigned int size1 = sizeof(s1), size2 = sizeof(s2), size = size1 + size2 - 1;
+	unsigned int size1, size2, size;
 	unsigned int i, j, k = 0;
 
 	if (s1 == NULL)
 		size1 = 0;
+	else
+		size1 = sizeof(s1);
 	if (s2 == NULL)
 		size2 = 0;
+	else
+		size2 = sizeof(s2);
+	size = size1 + size2 - 1;
 	ptr = malloc(size);
+
 	if (ptr == NULL)
 		return (NULL);
+
 	for (i = 0; i < size1 - 1; i++)
 	{
 		ptr[i] = s1[i];
