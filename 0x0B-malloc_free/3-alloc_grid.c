2a0a6456dc37504a4315f5c77373997097fc2bad
Duaarabie <Duaarabie@gmail.com>
2023-09-04 13:34:10 -0700

    commit all

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	array = malloc(height * sizeof(int));
+	array = malloc(height * sizeof(int*));
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
