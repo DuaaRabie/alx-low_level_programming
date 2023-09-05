2d323028c65da87d4eeb577bb63fcfd13f378ce9
Duaarabie <Duaarabie@gmail.com>
2023-09-04 17:15:15 -0700

    commit all

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,22 +13,22 @@ char **strtow(char *str)
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
-
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] != ' ' && str[i - 1] == ' ')
 			words++;
 	}
-	split = malloc(sizeof(char*) * words + 1);
-
+	split = malloc(sizeof(char *) * words + 1);
+	if (split == NULL)
+		return (NULL);
 	for (i = 0, j = 0; str[i] != '\0'; i++)
 	{
-		if (str[k] == ' ' && (i == 0 || str[i - 1] == ' ' ))
+		if (str[i] == ' ' && (i == 0 || str[i - 1] == ' '))
 		{
 			int len = 0;
+
 			while (str[i + len] != ' ' && str[i + len] != '\0')
 				len++;
-
 			split[j] = malloc(sizeof(char) * (len + 1));
 			if (split[j] == NULL)
 			{
@@ -37,16 +37,13 @@ char **strtow(char *str)
 				free(split);
 				return (NULL);
 			}
-
 			for (k = 0; k < len; k++)
 				split[j][k] = str[i + k];
 			split[j][k] = '\0';
 			j++;
 		}
 	}
+	split[j] = NULL;
+	return (split);
 
-	split[j][0] = '\n';
-
-	return (split);	
-	
 }
