8e61bc08a3a81c38a0da55c559ca27beeec01146
Duaarabie <Duaarabie@gmail.com>
2023-09-07 06:24:36 -0700

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,10 +25,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 	}
 
-	new = ptr;
+	/*new = ptr;
 	new = malloc(new_size);
 
 	free(ptr);
-
+*/
 	return (new);
 }
