34e44735530217f144339a2a1385b45996e73b4d
Duaarabie <Duaarabie@gmail.com>
2023-09-07 02:25:00 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	size1 = strlen(s1);
 	size2 = strlen(s2);
 	if (n >= size2)
-		size = size1 + size2;
+		size = size1 + size2 + 1;
 	else
 		size = size1 + n + 1;
 
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptr[i] = s1[i];
 	}
-	for (; j <= n; i++)
+	for (; j <= n && s2[j] != '\0'; i++)
 	{
 		ptr[i] = s2[j];
 		j++;
