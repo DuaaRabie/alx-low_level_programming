44a2a7fa228017b2e5ad8ad576f12e6a893e810f
Duaarabie <Duaarabie@gmail.com>
2023-09-07 03:47:54 -0700

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *ptr;
-	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -17,9 +16,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
-	       ptr[i] = 0;
-	ptr = (void *) ptr;
 	
 	return (ptr);
 }
