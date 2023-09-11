d832f586e093cffcbdc7e8d05794e5beb15dfc28
Duaarabie <Duaarabie@gmail.com>
2023-09-11 07:51:37 -0700

    create a function to free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,8 +22,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->name = strdup(name);
 	d->owner = strdup(owner);
 	d->age = age;
-	if (d->name == NULL || d->age == '\0' || d->owner == NULL)
-		return (NULL);
 
 	return (d);
 }
