144bf9c5bade51f67849b8db17366eda28fc3b5f
Duaarabie <Duaarabie@gmail.com>
2023-09-11 04:45:28 -0700

    update code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H_
 #define DOH_H_
 
+/**
+ * struct dog - type for dog
+ * @name: dog name
+ * @age: dog age
+ * #owner: dog owner
+ *
+ * Description: a new type called dog that includes
+ * 		dog name, age and owner
+ */
 struct dog
 {
 	char *name;
