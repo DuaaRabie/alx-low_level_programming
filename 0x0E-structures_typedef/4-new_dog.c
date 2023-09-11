c90d796713c7b464e9a5f1d9254616476ef67b4d
Duaarabie <Duaarabie@gmail.com>
2023-09-11 08:15:22 -0700

    update code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,16 +19,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-
+	d->name = malloc(sizeof(char) * strlen(name) + 1);
 	d->name = strdup(name);
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-
 	d->age = age;
 
+	d->owner = malloc(sizeof(char) * strlen(owner) + 1);
 	d->owner = strdup(owner);
 	if (d->owner == NULL)
 	{
