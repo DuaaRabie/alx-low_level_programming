3e16ba8f0a8817b14a95bea5ac78fe6fb4fdc8cf
Duaarabie <Duaarabie@gmail.com>
2023-09-11 06:16:24 -0700

    create a function that creates new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - creats varible of type dog
+ * @name: the name value
+ * @age: the age value
+ * @owner: the owner value
+ * Return: pointer to the dog struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	char *dog_name = name;
+	char *dog_owner = owner;
+
+	d = malloc(sizeof(dog_t));
+	(*d).name = dog_name;
+	(*d).age = age;
+	(*d).owner = dog_owner;
+
+	return (d);
+}
