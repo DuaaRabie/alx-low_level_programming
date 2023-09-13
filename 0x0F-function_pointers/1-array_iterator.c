c1bb35874c8850e8b5a51555b6519110afcc045c
Duaarabie <Duaarabie@gmail.com>
2023-09-13 03:43:30 -0700

    create a function that execute another function on array elements

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - excute a given function in all elements of given array
+ * @array: address of the array
+ * @size: size of the array
+ * @action: pointer to the function to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action != NULL)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
