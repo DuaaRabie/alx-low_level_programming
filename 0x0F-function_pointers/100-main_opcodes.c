568824dc5bfd4fbb2a607584fb8931fefb981fca
Duaarabie <Duaarabie@gmail.com>
2023-09-13 10:05:42 -0700

    create a program prints opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int nb = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (nb < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	while (nb)
+	{
+		printf("%02hhx%s", *opc, " ");
+		nb--;
+	}
+	printf("\n");
+
+	return (0);
+}
