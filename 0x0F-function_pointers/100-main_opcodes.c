f235189a6b7cbb639d5719cca996dcd6d8986869
Duaarabie <Duaarabie@gmail.com>
2023-09-13 11:02:07 -0700

    update code

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,17 +12,17 @@ int main(int argc, char *argv[])
 	char *opc = (char *)main;
 	int nb = atoi(argv[1]);
 
-	if (argc != 2)
+	/*if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
-	}
+	}*/
 
-	/*if (nb < 0)
+	if (nb < 0)
 	{
 		printf("Error\n");
 		exit(2);
-	}*/
+	}
 
 	while (nb)
 	{
