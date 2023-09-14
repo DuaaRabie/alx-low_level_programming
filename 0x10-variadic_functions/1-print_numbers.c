5c8b7d9022884ce7ea1f4c87bd5e437961733e9a
Duaarabie <Duaarabie@gmail.com>
2023-09-14 12:29:58 -0700

    create printing variadic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print all arguments
+ * @separator: string to be printed between numbers
+ * @n: number of integers to be printed
+ * ...: variadic arguments
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+
+	if (separator != NULL)
+	{
+		va_start(num, n);
+		for (i = 0; i < n; i++)
+			printf("%d%s", va_arg(num, int), i == n - 1 ? "\n" : ", ");
+	}
+	va_end(num);
+}
