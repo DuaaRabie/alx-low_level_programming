620f727d7c126d52f8cd5bc303c153a9e4065739
Duaarabie <Duaarabie@gmail.com>
2023-09-14 14:44:20 -0700

    update code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,40 +12,37 @@ void print_all(const char * const format, ...)
 	unsigned int argn = 0, j = 0, i = 0;
 	char *s;
 
-	if (format != NULL)
+	while (format[i] != '\0')
 	{
-		while (format[i] != '\0')
+		argn += 1;
+		i++;
+	}
+	va_start(var, format);
+	while (j < argn)
+	{
+		switch (format[j])
 		{
-			argn += 1;
-			i++;
+			case 'c':
+				printf("%c%s", va_arg(var, int), j == argn - 1 ? "\n" : ", ");
+				break;
+			case 'i':
+				printf("%d%s", va_arg(var, int), j == argn - 1 ? "\n" : ", ");
+				break;
+			case 'f':
+				printf("%f%s", va_arg(var, double), j == argn - 1 ? "\n" : ", ");
+				break;
+			case 's':
+				s = va_arg(var, char*);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", s, j == argn - 1 ? "\n" : ", ");
+				break;
+			default:
+				if (j == argn - 1)
+					printf("\n");
+				break;
 		}
-		va_start(var, format);
-		while (j < argn)
-		{
-			switch (format[j])
-			{
-				case 'c':
-					printf("%c%s", va_arg(var, int), j == argn - 1 ? "\n" : ", ");
-					break;
-				case 'i':
-					printf("%d%s", va_arg(var, int), j == argn - 1 ? "\n" : ", ");
-					break;
-				case 'f':
-					printf("%f%s", va_arg(var, double), j == argn - 1 ? "\n" : ", ");
-					break;
-				case 's':
-					s = va_arg(var, char*);
-					if (s == NULL)
-						s = "(nil)";
-					printf("%s%s", s, j == argn - 1 ? "\n" : ", ");
-					break;
-				default:
-					if (j == argn - 1)
-						printf("\n");
-					break;
-			}
-			j++;
-		}
+		j++;
 	}
 	va_end(var);
 }
