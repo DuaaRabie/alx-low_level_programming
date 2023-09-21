10b38900954046d3c7864a471615b074a95912d9
DuaaRabie <Duaarabie11@gmail.com>
2023-09-21 08:07:37 -0700

    print list

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -10,12 +10,12 @@
  */
 int main(void)
 {
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
+	list_t *head;
+	list_t *new;
+	list_t hello = {"World", 5, NULL};
+	size_t n;
 
-    head = &hello;
+    head &hello;
     new = malloc(sizeof(list_t));
     if (new == NULL)
     {
