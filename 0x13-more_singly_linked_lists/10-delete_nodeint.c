cf21cd800e9c05be515e08aeb0eb74a6657ebb69
DuaaRabie <Duaarabie11@gmail.com>
2023-09-26 07:46:08 -0700

    fdelete nelete node nth

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete a node in a given position
+ * @head: address of the first pointer
+ * @idx: position of node
+ * Return: 1 success | 0 fail
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int idx)
+{
+	listint_t *temp1 = *head;
+	listint_t *temp2;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	temp2 = temp1->next;
+
+	if (idx == 0)
+	{
+		*head = temp2;
+		free(temp1);
+		return (1);
+	}
+
+	while (i < idx - 1)
+	{
+		if (temp1 == NULL)
+			return (-1);
+		temp1 = temp1->next;
+		temp2 = temp1->next;
+		i++;
+	}
+	temp1->next = temp2->next;
+	free(temp2);
+
+	return (1);
+}
