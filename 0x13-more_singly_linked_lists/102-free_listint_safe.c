908da7e19dc484895e4032de475e1fbd6befd490
DuaaRabie <Duaarabie11@gmail.com>
2023-09-27 15:55:17 -0700

    update code

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,17 +7,34 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	int count = 0;
-	listint_t *temp = *h;
+	int count = 0, i = 0, j = 0 ;
+	listint_t *temp = *h, *next, *array[200], *loop = NULL;
 
-	if (h == NULL || *h)
+	if (h == NULL || *h == NULL)
 		return (count);
 
-	do {
+
+	while (temp != loop)
+	{
+		j = 0;
+		array[i] = temp;
+		array[i + 1] = NULL;
+		while (array[j])
+		{
+			if (temp->next == array[j])
+			{
+				loop = array [j];
+				break;
+			}
+			j++;
+
+		}
+		i++;
+		next = temp->next;
 		free(temp);
 		count++;
-		temp = temp->next;
-	} while (temp != *h);
+		temp = next;
+	}
 
 	*h = NULL;
 
