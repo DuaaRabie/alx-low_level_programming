97d63afd6e7914ab2fb68776f1a9ddf68a14b134
DuaaRabie <Duaarabie11@gmail.com>
2023-09-28 11:34:07 -0700

    get bit value

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * digits - calculate the total binary digits
+ * @n: the number
+ * Return: total number of digits
+ */
+int digits(unsigned long int n)
+{
+	unsigned long int power = 1, highest = 1;
+	int digit = 1;
+
+	while (n > highest)
+	{
+		digit++;
+		power *= 2;
+		highest += power;
+	}
+
+	return (digit);
+}
+
+/**
+ * get_bit - get the value of a bit at a given index
+ * @n: the number
+ * @index: the index of bit
+ * Return: the value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+	int total = digits(n);
+	char *b = (char *)calloc(total, sizeof(char));
+
+	if (n == 0)
+		putchar('0');
+
+	while (n)
+	{
+		b[i] = (n & 1);
+		n = n >> 1;
+		if (i == index)
+			return (b[i]);
+		i++;
+	}
+
+	return (-1);
+}
