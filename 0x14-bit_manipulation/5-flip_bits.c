c1c88e83ad89e6f0b1cab5aeefca9029fac9a4ab
DuaaRabie <Duaarabie11@gmail.com>
2023-09-28 18:04:50 -0700

    check number of bits to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - check number of bits to filp between two numbers
+ * @n: the first number
+ * @m: the second number
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int total = (sizeof(unsigned long int) * 8) - 1;
+	unsigned int flips = 0;
+
+	while (total--)
+	{
+		if ((m & 1) != (n & 1))
+		{
+			flips++;
+		}
+		m = (m >> 1);
+		n = (n >> 1);
+	}
+
+	return (flips);
+}
