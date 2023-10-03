73d7dfd8c3bbcbae8138f0d927430413844fa680
DuaaRabie <Duaarabie11@gmail.com>
2023-10-02 20:05:12 -0700

    update code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,13 +18,13 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", av[1], av[2]);
 		exit(97);
 	}
-	dfrom = open("test.txt", O_RDONLY);
+	dfrom = open(av[1], O_RDONLY);
 	if (dfrom == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	dto = open("test.txt", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	dto = open(av[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (dto == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
