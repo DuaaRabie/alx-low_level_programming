e6f82cba5c7f2984d3e4b40ffea0a091b3e1f2c9
DuaaRabie <Duaarabie11@gmail.com>
2023-10-02 19:54:37 -0700

    copy file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * copy_file - copies file to another file
@@ -8,39 +9,46 @@
  */
 int main(int ac, char *av[])
 {
-	int dfrom, dto, close_check;
+	int dfrom, dto, close_check, rcount = 1, wcount = 0;
 	char data[1024];
-
+	size_t tcount = 0;
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", av[1], av[2]);
 		exit(97);
 	}
-
 	dfrom = open("test.txt", O_RDONLY);
 	if (dfrom == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	dto = open("test.txt", O_WRONLY | O_TRUNC);
 	if (dto == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
 		exit(98);
 	}
-
-
-
+	while (rcount != 0)
+	{
+		rcount = read(dfrom, data, 1024);
+		if (rcount == 0)
+			return (tcount);
+		if (rcount == -1)
+			return (0);
+		wcount = write(dto, data, rcount);
+		if (wcount == -1)
+			return (0);
+		if (wcount != rcount)
+			return (0);
+	}
 	close_check = close(dfrom);
 	if (close_check == 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dfrom);
 		exit(100);
 	}
-
 	close_check = close(dto);
 	if (close_check == 0)
 	{
@@ -48,5 +56,5 @@ int main(int ac, char *av[])
 		exit(100);
 	}
 
-
+	return (0);
 }
