7cf21526c5e01d5291ad626e0b5a756fd3a3f3d6
DuaaRabie <Duaarabie11@gmail.com>
2023-10-02 17:53:59 -0700

    update code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char data[BUFSIZ];
 	ssize_t rcount, wcount = 0;
-	size_t tcount = 0;
+	size_t tcount = 0, size;
 
 	if (filename == NULL)
 		return (0);
@@ -22,12 +22,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	while (tcount < letters)
 	{
-		rcount = read(fd, data, letters - wcount > BUFSIZ ? BUFSIZ : letters - tcount);
+		if (letters - wcount > BUFSIZ)
+			size = BUFSIZ;
+		else
+			size = letters - tcount;
+
+		rcount = read(fd, data, size);
 		if (rcount == 0)
 			return (tcount);
+		if (rcount == -1)
+			return (0);
 
 		wcount = write(STDOUT_FILENO, data, rcount);
-		if (wcount == 0)
+		if (wcount == -1)
 			return (0);
 
 		tcount += rcount;
