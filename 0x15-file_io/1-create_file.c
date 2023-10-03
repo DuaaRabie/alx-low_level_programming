3f6d42779eaf5a97e1fb80244cec02ade69e544d
DuaaRabie <Duaarabie11@gmail.com>
2023-10-02 18:38:42 -0700

    update code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (*text_content != '\0')
+	while (*text_content != '\0' && text_content != NULL)
 	{
 		wcount += write(fd, text_content, 1);
 		if (wcount == -1)
