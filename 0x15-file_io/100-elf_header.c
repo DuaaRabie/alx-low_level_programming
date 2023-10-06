e9e375a2686fe90dbd72d1b414b55cecc10fe051
DuaaRabie <Duaarabie11@gmail.com>
2023-10-05 20:14:21 -0700

    update code

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -26,7 +26,7 @@ void print_magic_class_data(Elf64_Ehdr elfh)
 	switch (elfh.e_ident[EI_CLASS])
 	{
 		case ELFCLASSNONE:
-			printf("NONE\n");
+			printf("none\n");
 			break;
 		case ELFCLASS32:
 			printf("ELF32\n");
@@ -39,7 +39,7 @@ void print_magic_class_data(Elf64_Ehdr elfh)
 	switch (elfh.e_ident[EI_DATA])
 	{
 		case ELFDATANONE:
-			printf("NONE\n");
+			printf("none\n");
 			break;
 		case ELFDATA2LSB:
 			printf("2's complement, little endian\n");
@@ -64,10 +64,12 @@ void print_version(Elf64_Ehdr elfh)
 			printf(" (current)\n");
 			break;
 		case EV_NONE:
-			printf(" \n");
+			printf("\n");
 			break;
 	}
 }
+
+void print_more_os(Elf64_Ehdr elfh);
 
 /**
  * print_os - prints os/abi
@@ -83,25 +85,25 @@ void print_os(Elf64_Ehdr elfh)
 			printf("UNIX - System V\n");
 			break;
 		case ELFOSABI_HPUX:
-			printf("HP-UX ABI\n");
+			printf("UNIX - HP-UX\n");
 			break;
 		case ELFOSABI_NETBSD:
 			printf("UNIX - NetBSD\n");
 			break;
 		case ELFOSABI_LINUX:
-			printf("Linux ABI\n");
+			printf("UNIX - Linux\n");
 			break;
 		case ELFOSABI_SOLARIS:
 			printf("UNIX - Solaris\n");
 			break;
 		case ELFOSABI_IRIX:
-			printf("IRIX ABI\n");
+			printf("UNIX - IRIX\n");
 			break;
 		case ELFOSABI_FREEBSD:
-			printf("FreeBSD ABI\n");
+			printf("UNIX - FreeBSD\n");
 			break;
 		case ELFOSABI_TRU64:
-			printf("TRU64 UNIX ABI\n");
+			printf("UNIX - TRU64\n");
 			break;
 		case ELFOSABI_ARM:
 			printf("ARM architecture ABI\n");
@@ -109,8 +111,36 @@ void print_os(Elf64_Ehdr elfh)
 		case ELFOSABI_STANDALONE:
 			printf("Stand-alone (embedded) ABI\n");
 			break;
+		default:
+			print_more_os(elfh);
+			break;
 	}
 }
+
+/**
+ * print_more_os - prints more os
+ * @elfh: elf header
+ * Return: nothing
+ */
+void print_more_os(Elf64_Ehdr elfh)
+{
+	switch (elfh.e_ident[EI_OSABI])
+	{
+		case ELFOSABI_AIX:
+			printf("UNIX - AIX\n");
+			break;
+		case ELFOSABI_MODESTO:
+			printf("Nove11 - Modesto\n");
+			break;
+		case ELFOSABI_OPENBSD:
+			printf("UNIX - OpenBSD\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", elfh.e_ident[EI_OSABI]);
+			break;
+	}
+}
+
 /**
  * print_type - prints type
  * @elfh: elf header
@@ -118,11 +148,13 @@ void print_os(Elf64_Ehdr elfh)
  */
 void print_type(Elf64_Ehdr elfh)
 {
-	unsigned char *hex = (unsigned char *)&elfh.e_entry;
+	char *type = (char *)&elfh.e_type;
 	int i = 0;
 
 	printf("  Type:                              ");
-	switch (elfh.e_type)
+	if (elfh.e_ident[EI_DATA] == ELFDATA2MSB)
+		i = 1;
+	switch (type[i])
 	{
 		case ET_NONE:
 			printf("NONE (None)\n");
@@ -139,15 +171,43 @@ void print_type(Elf64_Ehdr elfh)
 		case ET_CORE:
 			printf("CORE (Core file)\n");
 			break;
+		default:
+			printf("<unknown: %x\n", type[i]);
+			break;
 	}
+}
+
+/**
+ * print_entry - prints entry
+ * @elfh: elf header
+ * Return: nothing
+ */
+void print_entry(Elf64_Ehdr elfh)
+{
+	unsigned char *hex = (unsigned char *)&elfh.e_entry;
+	int i = 0, len = 0;
+
 	printf("  Entry point address:               0x");
-	i = elfh.e_ident[EI_CLASS] == ELFCLASS64 ? 7 : 3;
-	while (!hex[i])
-		i--;
-	printf("%x", hex[i--]);
-	for (; i >= 0; i--)
-		printf("%02x", hex[i]);
-	printf("\n");
+
+	if (elfh.e_ident[EI_DATA] != ELFDATA2MSB)
+	{
+		i = elfh.e_ident[EI_CLASS] == ELFCLASS64 ? 7 : 3;
+		while (!hex[i])
+			i--;
+		printf("%x", hex[i--]);
+		for (; i >= 0; i--)
+			printf("%02x", hex[i]);
+	}
+	else
+	{
+		len = elfh.e_ident[EI_CLASS] == ELFCLASS64 ? 7 : 3;
+		while (!hex[i])
+			i++;
+		printf("%x", hex[i++]);
+		for (; i <= len; i++)
+			printf("%02x", hex[i]);
+	}
+			printf("\n");
 }
 
 /**
@@ -164,7 +224,7 @@ int main(int ac, char **av)
 
 	if (ac != 2)
 	{
-		dprintf(STDERR_FILENO, "elf_header: Warning: Nothing to do.\n");
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
 		exit(98);
 	}
 
@@ -175,9 +235,9 @@ int main(int ac, char **av)
 		exit(98);
 	}
 	size = read(fd, &elfh, sizeof(elfh));
-	if (size == -1 || size != sizeof(elfh))
+	if (size < 1 || size != sizeof(elfh))
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
 		exit(98);
 	}
 	if (elfh.e_ident[0] != 0x7f || elfh.e_ident[1] != 'E')
@@ -192,5 +252,6 @@ int main(int ac, char **av)
 	printf("  ABI Version:                       ");
 	printf("%d\n", elfh.e_ident[EI_ABIVERSION]);
 	print_type(elfh);
+	print_entry(elfh);
 	return (0);
 }
