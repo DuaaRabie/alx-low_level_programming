f3fdca41b340a926492f006319ea095a70118d0b
DuaaRabie <Duaarabie11@gmail.com>
2023-11-09 17:24:33 -0800

    update code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		return (new);
 	}
-	while (i < idx && temp != NULL)
+	while (i < idx)
 	{
 		if (temp == NULL)
 		{
