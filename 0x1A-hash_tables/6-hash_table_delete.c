51b9566093b9a07585ae5ead7dd6d6b8fc98b5b2
DuaaRabie <Duaarabie11@gmail.com>
2023-12-01 18:54:01 -0800

    update code

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete hash table
+ * @ht: pointer to the hash table
+ *
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			temp = ht->array[i];
+			ht->array[i] = ht->array[i]->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+}
