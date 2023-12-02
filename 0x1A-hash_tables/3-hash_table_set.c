e824edbcac73fee00e1e86502cc19cc00cc64665
DuaaRabie <Duaarabie11@gmail.com>
2023-12-01 17:08:06 -0800

    update code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -58,7 +58,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[index])
 	{
-		if (key != ht->array[index]->key)
+		if (key == ht->array[index]->key)
 		{
 			ht->array[index] = new;
 			new->next = NULL;
