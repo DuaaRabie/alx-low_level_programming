5346ecdf31153ecc41014d3a66e1fc1fc9dcc8fd
DuaaRabie <Duaarabie11@gmail.com>
2023-12-01 17:14:26 -0800

    update code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -58,14 +58,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[index])
 	{
-		if (key == ht->array[index]->key)
-		{
-			temp = ht->array[index];
-			ht->array[index] = new;
-			new->next = NULL;
-			free(temp);
-		}
-		else
+		if (key != ht->array[index]->key)
 		{
 			temp = ht->array[index];
 			ht->array[index] = new;
