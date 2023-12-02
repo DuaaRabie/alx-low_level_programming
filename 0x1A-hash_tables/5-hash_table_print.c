f90a791c8d348a291e93372d9cee54a109863d3c
DuaaRabie <Duaarabie11@gmail.com>
2023-12-01 18:43:31 -0800

    task5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the hash table
+ * @ht: pointer to the hash table
+ * 
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	long unsigned int i;
+	hash_node_t *temp;
+
+	if (ht)
+	{
+		printf("{");
+
+		for (i = 0; i < ht->size; i++)
+		{
+			temp = ht->array[i];
+
+			while (temp)
+			{
+				printf("'%s': '%s'", temp->key, temp->value);
+				if (temp->next && temp->next->next)
+					printf(", ");
+				temp = temp->next;
+			}
+			if (temp && i != 0)
+				printf(", ");
+		}
+		printf("}\n");
+	}
+}
