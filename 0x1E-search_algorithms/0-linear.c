eecafa806736c4fa416be12de0860fa1859725c0
Duaa Rabie <DuaaRabie11@gmail.com>
2024-05-08 04:06:30 -0700

    task0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * linear_search - searches for a value in an array of
+ *		   integers using the Linear search algorithm
+ * @array: array of elements
+ * @size: number of array elements
+ * @value: value to search for
+ * Return: the index of the value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (value);
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef SEARCH_H_
+#define SEARCH_H_
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
